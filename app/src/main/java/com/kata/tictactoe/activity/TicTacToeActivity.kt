83a2d602e560f2579fc895def15d7c8470e77dd0
gregorynowik <gregorynowik@gmail.com>
2021-09-21 13:02:32 +0200

    added pawns display and Glide library
    added paddings to the drawables

--- app/src/main/java/com/kata/tictactoe/activity/TicTacToeActivity.kt
+++ app/src/main/java/com/kata/tictactoe/activity/TicTacToeActivity.kt
@@ -5,6 +5,7 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.kata.tictactoe.R
 import com.kata.tictactoe.databinding.TicTacToeActivityBinding
 import com.kata.tictactoe.manager.TicTacToeManager
@@ -31,31 +32,31 @@ class TicTacToeActivity : AppCompatActivity() {
 
         binding?.apply {
             topLeftImageView.apply {
-                setOnClickListener { addPawn(this, 0, 0)  }
+                setOnClickListener { addPawn(this, 0, 0) }
             }
             topCenterImageView.apply {
-                setOnClickListener { addPawn(this, 1, 0)  }
+                setOnClickListener { addPawn(this, 1, 0) }
             }
             topRightImageView.apply {
-                setOnClickListener { addPawn(this, 2, 0)  }
+                setOnClickListener { addPawn(this, 2, 0) }
             }
             centerLeftImageView.apply {
-                setOnClickListener { addPawn(this, 0, 1)  }
+                setOnClickListener { addPawn(this, 0, 1) }
             }
             centerCenterImageView.apply {
-                setOnClickListener { addPawn(this, 1, 1)  }
+                setOnClickListener { addPawn(this, 1, 1) }
             }
             centerRightImageView.apply {
-                setOnClickListener { addPawn(this, 2, 1)  }
+                setOnClickListener { addPawn(this, 2, 1) }
             }
             bottomLeftImageView.apply {
-                setOnClickListener { addPawn(this, 0, 2)  }
+                setOnClickListener { addPawn(this, 0, 2) }
             }
             bottomCenterImageView.apply {
-                setOnClickListener { addPawn(this, 1, 2)  }
+                setOnClickListener { addPawn(this, 1, 2) }
             }
             bottomRightImageView.apply {
-                setOnClickListener { addPawn(this, 2, 2)  }
+                setOnClickListener { addPawn(this, 2, 2) }
             }
 
             resetButton.setOnClickListener { viewModel.reset() }
@@ -63,13 +64,21 @@ class TicTacToeActivity : AppCompatActivity() {
     }
 
     private fun addPawn(imageView: ImageView, x: Int, y: Int) {
+        // retrieve player turn before it is changed
+        val currentPlayerTurn = viewModel.currentPlayerTurn
         val success = viewModel.addPoint(x, y)
 
         if (success) {
-            val res = when(viewModel.currentPlayerTurn){
+            // pawn drawable
+            val res = when (currentPlayerTurn) {
                 TicTacToeManager.PlayerTurn.X -> ContextCompat.getDrawable(this, R.drawable.pawn_x)
-                TicTacToeManager.PlayerTurn.O -> ContextCompat.getDrawable(this, R.drawable.pawn_circle)
+                TicTacToeManager.PlayerTurn.O -> ContextCompat.getDrawable(
+                    this,
+                    R.drawable.pawn_circle
+                )
             }
+
+            Glide.with(this).load(res).into(imageView)
 
 
         }
